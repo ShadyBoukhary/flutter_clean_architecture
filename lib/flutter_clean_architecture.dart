ddad9e37fd407da47b49297ecaa81433834393fb
Shady Boukhary <sb199898.sb@gmail.com>
2019-04-27 18:52:44 -0500

    Export source files

---
+++ lib/flutter_clean_architecture.dart
@@ -0,0 +1,11 @@
+library flutter_clean_architecture;
+
+export 'package:flutter_clean_architecture/src/controller.dart';
+export 'package:flutter_clean_architecture/src/observer.dart';
+export 'package:flutter_clean_architecture/src/presenter.dart';
+export 'package:flutter_clean_architecture/src/usecase.dart';
+export 'package:flutter_clean_architecture/src/view.dart';
+
+
+
+
