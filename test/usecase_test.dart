b8af04c9be104e689103dd60ead56a25553828f4
Shady Boukhary <sb199898.sb@gmail.com>
2019-04-29 21:21:34 -0500

    Created UseCase Unit test

---
+++ test/usecase_test.dart
@@ -0,0 +1,74 @@
+import 'package:test/test.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:rxdart/src/observables/observable.dart';
+
+void main() {
+  group('Domain modules', () {
+    test('UseCase onNext and onDone.', () async {
+      CounterUseCaseObserver observer = CounterUseCaseObserver();
+      CounterUseCase().execute(observer);
+      await Future.delayed(Duration(milliseconds: 1000), () {
+        expect(observer.number, 2);
+        expect(observer.done, true);
+        expect(observer.error, false);
+      });
+    });
+
+    test('UseCase .OnError.', () async {
+      CounterUseCaseObserver observer = CounterUseCaseObserver();
+      CounterUseCaseError().execute(observer);
+      await Future.delayed(Duration(milliseconds: 1000), () {
+        expect(observer.number, -1);
+        expect(observer.done, true);
+        expect(observer.error, true);
+      });
+    });
+
+    test('UseCase .dispose cancels the subscription', () async {
+      CounterUseCaseObserver observer = CounterUseCaseObserver();
+      CounterUseCase usecase = CounterUseCase()
+      ..execute(observer);
+      await Future.delayed(Duration(milliseconds: 15), () {
+        usecase.dispose();
+        expect(observer.number, 0);
+        expect(observer.done, false);
+        expect(observer.error, false);
+      });
+    });
+  });
+}
+
+class CounterUseCase extends UseCase<int, void> {
+  @override
+  Future<Observable<int>> buildUseCaseObservable(void params) async {
+    return Observable.periodic(Duration(milliseconds: 10), (i) => i).take(3);
+  }
+}
+
+class CounterUseCaseError extends UseCase<int, void> {
+  @override
+  Future<Observable<int>> buildUseCaseObservable(void params) async {
+    return Observable.error(Error());
+  }
+}
+
+class CounterUseCaseObserver extends Observer<int> {
+  int number = -1;
+  bool done = false;
+  bool error = false;
+  @override
+  void onComplete() {
+    done = true;
+  }
+
+  @override
+  void onError(e) {
+    error = true;
+  }
+
+  @override
+  void onNext(int number) {
+    this.number++;
+    expect(number, this.number);
+  }
+}
