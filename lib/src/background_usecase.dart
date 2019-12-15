83b44a32ebf2ead50485920d23c897607be33e1d
Shady Boukhary <sb199898.sb@gmail.com>
2019-12-15 17:47:28 -0600

    using subjects, still have to figure out static run

--- lib/src/background_usecase.dart
+++ lib/src/background_usecase.dart
@@ -9,21 +9,23 @@ import 'dart:async';
 // }
 
 enum BackgroundUseCaseState { idle, loading, calculating }
-
+typedef UseCaseTask = void Function(SendPort message);
 class BackgroundUseCaseMessage<T> {
-  Observable<T> obs;
-  SendPort sendPort;
-  Observer<T> observer;
-  BackgroundUseCaseMessage(this.obs, this.sendPort, this.observer);
+  T data;
+  Error error;
+
+  BackgroundUseCaseMessage({this.data, this.error});
 }
 
 abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
+  BehaviorSubject<T> _subject;
   BackgroundUseCaseState _state = BackgroundUseCaseState.idle;
   Isolate _isolate;
   final ReceivePort _receivePort;
 
   BackgroundUseCase()
       : _receivePort = ReceivePort(),
+        _subject = BehaviorSubject(),
         super() {
     _receivePort.listen(_handleMessage);
   }
@@ -36,13 +38,10 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
   void execute(Observer<T> observer, [Params params]) async {
     if (!isRunning) {
       _state = BackgroundUseCaseState.loading;
-      Observable<T> obs = await buildUseCaseObservable(params);
-
-      BackgroundUseCaseMessage<T> message = BackgroundUseCaseMessage(
-          obs, _receivePort.sendPort, observer);
-      a = b;
-
-      _isolate = await Isolate.spawn<BackgroundUseCaseMessage>(_run, null);
+      _subject.listen(observer.onNext, onError: observer.onError, onDone: observer.onComplete);
+      _run = buildUseCaseTask(params);
+      _run = hi;
+      _isolate = await Isolate.spawn<SendPort>(_run, _receivePort.sendPort);
       if (!isRunning) {
         _isolate.kill(priority: Isolate.immediate);
       } else {
@@ -50,8 +49,23 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
       }
     }
   }
-  static Function a;
-  void b() {print(1);}
+
+  @override
+  Future<Observable<T>> buildUseCaseObservable(Params params) async {  }
+
+
+  UseCaseTask buildUseCaseTask(Params params) {
+    return (SendPort port) {
+      print(1);
+      BackgroundUseCaseMessage<T> message = BackgroundUseCaseMessage();
+      port.send(message);
+    };
+  }
+
+  static void hi(SendPort port) {
+
+  }
+
   @override
   void dispose() {
     stop();
@@ -60,7 +74,6 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
   void stop() {
     if (isRunning) {
       _state = BackgroundUseCaseState.idle;
-      dispose();
       if (_isolate != null) {
         _isolate.kill(priority: Isolate.immediate);
         _isolate = null;
@@ -68,20 +81,30 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
       }
     }
   }
+  static UseCaseTask _run;
 
-  static void _run(BackgroundUseCaseMessage message) {
-    a();
-    // final SendPort sender = message.sendPort;
-    // Observable obs = message.obs;
-    // Observer observer = message.observer;
-    // final StreamSubscription subscription = obs.listen(observer.onNext,
-    //     onDone: observer.onComplete, onError: observer.onError);
-    // sender.send(subscription);
-  }
+  // static void _run(BackgroundUseCaseMessage message) {
+  //   // final SendPort sender = message.sendPort;
+  //   // Observable obs = message.obs;
+  //   // Observer observer = message.observer;
+  //   // final StreamSubscription subscription = obs.listen(observer.onNext,
+  //   //     onDone: observer.onComplete, onError: observer.onError);
+  //   // sender.send(subscription);
+  // }
 
-  void _handleMessage(dynamic subscription) {
-    StreamSubscription sub = subscription as StreamSubscription;
-    addSubscription(sub);
+  void _handleMessage(dynamic message) {
+    assert(message is BackgroundUseCaseMessage);
+    var msg = message as BackgroundUseCaseMessage;
+    print("Received");
+    if (msg.data != null) {
+      assert(msg.data is T);
+      _subject.add(msg.data);
+    } else if (msg.error != null) {
+      _subject.addError(msg.error);
+    } else {
+      // TODO: warning
+    }
+
 
   }
 }
