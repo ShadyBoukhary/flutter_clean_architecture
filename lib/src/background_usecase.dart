d230d1818831ad6c8cb5d25ad1ab4a2e82126a17
Shady Boukhary <sb199898.sb@gmail.com>
2019-12-15 19:44:41 -0600

    Working backgroundusecase, wrote tests

--- lib/src/background_usecase.dart
+++ lib/src/background_usecase.dart
@@ -4,17 +4,20 @@ import 'package:meta/meta.dart';
 import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 
-// abstract class BackgroundObserver<T> extends Observer<T> {
-
-// }
-
 enum BackgroundUseCaseState { idle, loading, calculating }
-typedef UseCaseTask = void Function(SendPort message);
+typedef UseCaseTask = void Function(BackgroundUseCaseParams message);
+
 class BackgroundUseCaseMessage<T> {
   T data;
   Error error;
+  bool done;
+  BackgroundUseCaseMessage({this.data, this.error, this.done = false});
+}
 
-  BackgroundUseCaseMessage({this.data, this.error});
+class BackgroundUseCaseParams<T> {
+  T params;
+  SendPort port;
+  BackgroundUseCaseParams(this.port, {this.params});
 }
 
 abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
@@ -22,6 +25,7 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
   BackgroundUseCaseState _state = BackgroundUseCaseState.idle;
   Isolate _isolate;
   final ReceivePort _receivePort;
+  static UseCaseTask _run;
 
   BackgroundUseCase()
       : _receivePort = ReceivePort(),
@@ -38,73 +42,58 @@ abstract class BackgroundUseCase<T, Params> extends UseCase<T, Params> {
   void execute(Observer<T> observer, [Params params]) async {
     if (!isRunning) {
       _state = BackgroundUseCaseState.loading;
-      _subject.listen(observer.onNext, onError: observer.onError, onDone: observer.onComplete);
+      _subject.listen(observer.onNext,
+          onError: observer.onError, onDone: observer.onComplete);
       _run = buildUseCaseTask(params);
-      _run = hi;
-      _isolate = await Isolate.spawn<SendPort>(_run, _receivePort.sendPort);
-      if (!isRunning) {
-        _isolate.kill(priority: Isolate.immediate);
-      } else {
-        _state = BackgroundUseCaseState.calculating;
-      }
+      Isolate.spawn<BackgroundUseCaseParams>(_run, BackgroundUseCaseParams(_receivePort.sendPort, params: params))
+          .then<void>((Isolate isolate) {
+        if (!isRunning) {
+          isolate.kill(priority: Isolate.immediate);
+        } else {
+          _state = BackgroundUseCaseState.calculating;
+          _isolate = isolate;
+        }
+      });
     }
   }
 
   @override
-  Future<Observable<T>> buildUseCaseObservable(Params params) async {  }
-
-
-  UseCaseTask buildUseCaseTask(Params params) {
-    return (SendPort port) {
-      print(1);
-      BackgroundUseCaseMessage<T> message = BackgroundUseCaseMessage();
-      port.send(message);
-    };
+  @nonVirtual
+  Future<Observable<T>> buildUseCaseObservable(Params params) {
+    return null;
   }
 
-  static void hi(SendPort port) {
-
-  }
+  UseCaseTask buildUseCaseTask(Params params);
 
   @override
   void dispose() {
-    stop();
+    _stop();
     super.dispose();
   }
-  void stop() {
+
+  void _stop() {
     if (isRunning) {
       _state = BackgroundUseCaseState.idle;
       if (_isolate != null) {
         _isolate.kill(priority: Isolate.immediate);
         _isolate = null;
-        // TODO: add any more
       }
     }
   }
-  static UseCaseTask _run;
-
-  // static void _run(BackgroundUseCaseMessage message) {
-  //   // final SendPort sender = message.sendPort;
-  //   // Observable obs = message.obs;
-  //   // Observer observer = message.observer;
-  //   // final StreamSubscription subscription = obs.listen(observer.onNext,
-  //   //     onDone: observer.onComplete, onError: observer.onError);
-  //   // sender.send(subscription);
-  // }
 
   void _handleMessage(dynamic message) {
     assert(message is BackgroundUseCaseMessage);
     var msg = message as BackgroundUseCaseMessage;
-    print("Received");
     if (msg.data != null) {
       assert(msg.data is T);
       _subject.add(msg.data);
     } else if (msg.error != null) {
       _subject.addError(msg.error);
-    } else {
-      // TODO: warning
+      _subject.close();
     }
 
-
+    if (msg.done) {
+      _subject.close();
+    }
   }
 }
