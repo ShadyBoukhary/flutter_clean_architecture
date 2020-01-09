bdf1caee3de83668378dc828c377946f43f6ea56
Shady Boukhary <sb199898.sb@gmail.com>
2020-01-08 19:22:51 -0600

    Formatting and added option to enable debugging statements

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
 import './src/app/pages/home/home_view.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterCleanArchitecture.debugModeOn();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
--- lib/flutter_clean_architecture.dart
+++ lib/flutter_clean_architecture.dart
@@ -1,6 +1,8 @@
 library flutter_clean_architecture;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/src/controller.dart';
+import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 
 export 'package:flutter_clean_architecture/src/controller.dart';
@@ -10,11 +12,24 @@ export 'package:flutter_clean_architecture/src/usecase.dart';
 export 'package:flutter_clean_architecture/src/view.dart';
 export 'package:flutter_clean_architecture/src/background_usecase.dart';
 
-
 class FlutterCleanArchitecture {
   /// Retrieves a Controller from the widget tree if one exists
   /// Can be used in widgets that exist in pages and need to use the same controller
   static Con getController<Con extends Controller>(BuildContext context) {
     return Provider.of<Con>(context);
   }
+
+  static void debugModeOn() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((record) {
+      dynamic e = record.error;
+      print('${record.loggerName}: ${record.level.name}: ${record.message} ');
+      if (e != null) {
+        print(e.toString());
+      }
+    });
+
+    Logger.root.info("Logger initialized.");
+  }
+
 }
