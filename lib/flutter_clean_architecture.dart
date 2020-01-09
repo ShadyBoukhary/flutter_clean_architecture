7ff74c4f69247bbb4674e0fd419548453dc37ad5
Shady Boukhary <sb199898.sb@gmail.com>
2020-01-08 18:52:21 -0600

    Added ability to use same controller in child widgets through a static method that wraps provder.of

--- lib/flutter_clean_architecture.dart
+++ lib/flutter_clean_architecture.dart
@@ -1,4 +1,7 @@
 library flutter_clean_architecture;
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/src/controller.dart';
+import 'package:provider/provider.dart';
 
 export 'package:flutter_clean_architecture/src/controller.dart';
 export 'package:flutter_clean_architecture/src/observer.dart';
@@ -6,3 +9,12 @@ export 'package:flutter_clean_architecture/src/presenter.dart';
 export 'package:flutter_clean_architecture/src/usecase.dart';
 export 'package:flutter_clean_architecture/src/view.dart';
 export 'package:flutter_clean_architecture/src/background_usecase.dart';
+
+
+class FlutterCleanArchitecture {
+  /// Retrieves a Controller from the widget tree if one exists
+  /// Can be used in widgets that exist in pages and need to use the same controller
+  static Con getController<Con extends Controller>(BuildContext context) {
+    return Provider.of<Con>(context);
+  }
+}
