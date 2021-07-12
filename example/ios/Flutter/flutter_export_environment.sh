ebc33daca015fbda607903abc2d8f6fde58696b1
Rafael Carvalho Monteiro <contato.rdev@gmail.com>
2021-07-12 13:35:20 -0300

    feat: added flutter constant restriction

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/rafaelcmm/SDKs/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/rafaelcmm/Workspace/flutter_clean_architecture/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
