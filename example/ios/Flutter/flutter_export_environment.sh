d9eee1163be627217faa0d53ef50ac94aa02071d
Ahmet TOK <ahmet@zuzu.dev>
2024-07-19 12:51:52 +0300

    upgraded rxdart to 0.27 to make it compatible to work with the graphql_flutter package graphql_flutter: ^5.2.0-beta.7 which is also dart 3

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\sb199\flutter_clean\flutter_clean_architecture\example"
+export "FLUTTER_ROOT=/Users/arrrrny/fvm/versions/3.22.2"
+export "FLUTTER_APPLICATION_PATH=/Users/arrrrny/Developer/flutter_clean_architecture/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
